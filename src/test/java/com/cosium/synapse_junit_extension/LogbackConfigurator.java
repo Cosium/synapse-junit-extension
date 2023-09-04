02304dd90a0588cdb595bad8ee9902674a553584
Réda Housni Alaoui <reda-alaoui@hey.com>
2023-09-04 12:10:15 +0200

    Fix compilation

--- src/test/java/com/cosium/synapse_junit_extension/LogbackConfigurator.java
+++ src/test/java/com/cosium/synapse_junit_extension/LogbackConfigurator.java
@@ -2,8 +2,8 @@ package com.cosium.synapse_junit_extension;
 
 import ch.qos.logback.classic.BasicConfigurator;
 import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.core.Context;
 import java.util.Optional;
 
 /**
@@ -12,15 +12,14 @@ import java.util.Optional;
 public class LogbackConfigurator extends BasicConfigurator {
 
   @Override
-  public ExecutionStatus configure(Context context) {
+  public ExecutionStatus configure(LoggerContext context) {
     super.configure(context);
     Level rootLevel =
         Optional.ofNullable(System.getenv("SYNAPSE_JUNIT_EXTENSION_TEST_LOG_ROOT_LEVEL"))
             .map(Level::valueOf)
             .orElse(Level.OFF);
 
-    LoggerContext loggerContext = (LoggerContext) context;
-    loggerContext.getLogger("ROOT").setLevel(rootLevel);
+    context.getLogger(Logger.ROOT_LOGGER_NAME).setLevel(rootLevel);
     return ExecutionStatus.DO_NOT_INVOKE_NEXT_IF_ANY;
   }
 }
